7f8ab034e275f53816af5d0d468d15c35b0c25df
Mohammed Hussain <2152584+mhhussain@users.noreply.github.com>
2022-03-19 00:02:10 -0400

    cross app communication working

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const { v4: uuidv4 } = require('uuid');
 const axios = require('axios');
+const os = require('os');
 
 const _ = require('lodash');
 const logger = require('./logger').logger('app');
@@ -21,6 +22,28 @@ app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
 
+// STARTUP PROCESS
+const startup = async () => {
+  var i = 1;
+  logger.info(`Running startup process`);
+
+  const pingerRegistration = {
+    host: 'localhost',
+    port: 5002,
+    route: 'ping',
+  }
+
+  const reg = await axios.post(`http://localhost:5001/register`, { registration: pingerRegistration });
+
+  // interval process
+  const process = async () => {
+    logger.info(`Running process [${i++}]`);
+    const res = await axios.get(`http://localhost:5001/map`);
+    console.log(res.data);
+  };
+
+  const int = setInterval(process, 3000);
+};
 
 // ERRORS
 app.use((e, req, res, next) => {
@@ -38,3 +61,5 @@ app.use((e, req, res, next) => {
 app.listen(configs.PORT, () => {
   logger.info(`Listening at http://localhost:${configs.PORT}`);
 });
+
+startup();
